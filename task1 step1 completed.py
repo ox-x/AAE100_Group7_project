27e3285b14a11260a978828d3b53b954e0956896
ox-x <xtx_xtx@outlook.com>
2024-10-29 13:38:18 +0800

    Update task1 step1 completed.py

--- task1 step1 completed.py
+++ task1 step1 completed.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 
 show_animation = True
 
-# value = [A321neo,A330-900neo,A330-900]
+# value = [A321neo,A330-900neo,A350-900]
 A = ["A321neo","A330-900neo","A330-900"]
 cost = [0.0,0.0,0.0]
 Cf = 1
